01b548c9237d7dd677983b277b5f4a309d147e99
mpgreg <milesper@gmail.com>
2017-06-08 10:56:25 +0200

    Create setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-!./create-tf-conda-env.sh
+!bash ./create-tf-conda-env.sh
 
 !git clone -b leewyang_keras https://github.com/yahoo/TensorFlowOnSpark
 
-!./prepare-mnist-data.sh
+!bash ./prepare-mnist-data.sh
